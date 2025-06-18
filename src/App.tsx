06b4a6ec96cf3641442e18f09709cd6de0e24a76
Alexhander <sshmal02@mail.ru>
2025-06-18 16:09:31 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,17 @@ import './App.css';
 function App() {
   const [titleText, setTitleText] = useState('Задание 1');
 
-  const [elements, setElements] = useState<React.ReactNode[]>([]);
+  const [elements, setElements] = useState<number[]>([]);
 
   const handleChangeTitle = () => {
     setTitleText('Обновлённый заголовок');
   };
 
   const handleAddElement = () => {
-    setElements(prev => [
-      ...prev,
-      <p 
-      key={prev.length} className="text">
-      Новый элемент {prev.length + 1}
-      </p>
-    ]);
+    setElements(prev => [...prev, prev.length + 1]);
   };
 
-  return (
+  return ( //Структура JSX
     <>
       <h1 id="title">{titleText}</h1>
 
@@ -28,14 +22,13 @@ function App() {
       <p className="text">Второй параграф</p>
       <p className="text">Третий параграф</p>
 
-      <button id="changeBtn" onClick={handleChangeTitle}>
-        Изменить содержимое
-      </button>
-      <button id="addBtn" onClick={handleAddElement}>
-        Добавить элемент
-      </button>
-
-      <div id="container">{elements}</div>
+      <button id="changeBtn" onClick={handleChangeTitle}> Изменить содержимое </button>
+      <button id="addBtn" onClick={handleAddElement}> Добавить элемент </button>
+      <div id = "containder">
+      {elements.map((elem, index) => (
+        <div key = {index}> Новый элемент {elem}</div>
+      ))}
+      </div>
     </>
   );
 }
